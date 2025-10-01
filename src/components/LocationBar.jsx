a9637408549aa134bcdbe29df8eda2e672d2102a
EmmanuelNdati <aveliemmanuel@gmail.com>
2025-10-01 06:37:44 +0300

    seo

--- src/components/LocationBar.jsx
+++ src/components/LocationBar.jsx
@@ -2,14 +2,33 @@
 import React from "react";
 import { FaLocationDot, FaPhoneVolume } from "react-icons/fa6";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const LocationBar = () => {
   return (
     <div className="flex justify-center items-center bg-red-500 p-3 gap-5 text-white max-lg:flex-col">
       <FaLocationDot />
       <h1>Kiambu Road Near AAR Hospital</h1>
-      <FaPhoneVolume />
-      <h1 className="font-extrabold">0716 884 011</h1>
+      
+      <a
+            href="tel:+254716884011"
+            className="flex items-center gap-4  text-black px-6 py-4 rounded-full shadow-lg hover:scale-105 transition duration-300 ease-in-out cursor-pointer"
+            aria-label="Call CarKey Experts"
+          >
+            <motion.div
+              animate={{
+                x: [0, -2, 2, -2, 2, 0], // back and forth
+              }}
+              transition={{
+                duration: 0.8,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+            >
+              <FaPhoneVolume className="text-2xl" />
+            </motion.div>
+            <span className="font-bold">0716 884 011</span>
+          </a>
       <Link to="/" className="font-extrabold ">
         Home </Link>
 
